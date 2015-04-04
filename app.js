fa3e7592405251c8a43e9f9bb6fb3da366133b92
Sameet <sameetandpotatoes@gmail.com>
2015-04-04 15:45:14 -0500

    forgot to add express

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var request = require("request");
 var settings = require("./settings.js");
 var TwitterBot = require("node-twitterbot").TwitterBot;
+var express = require("express");
+var app = express();
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
 
 var headers = {
   "User-Agent": "callouscommits",
