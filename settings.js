f6985ae72373e919e23ece5e59493cba595087da
Sameet <sameetandpotatoes@gmail.com>
2015-04-05 19:24:02 -0500

    updated api credentials

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 exports.twitterAccess = {
-  "consumer_key": 'A2v40WghMntGsxuY7cVD3rhzc',
-  "consumer_secret": 'RIBCnzCr4xXI13Siekf5YWn5KsLbeSifvLF5NNJAB9y8wV9nzx',
+  "consumer_key": 'wXIq3R9QZjAgBllxlpzUbxYwI',
+  "consumer_secret": '23Ewmj7Ab6DZtaAdN28z1NyUQLXhwo7oJz1BpgkuTD8u9b4lZ2',
   "access_token": '3134504248-g7NQtbwt4iZqCJ3MsbzHmkKbwbYQBgnqbuqny7N',
   "access_token_secret": 'dUqCkEINoTN7NTjey5iDmkTK9METXEMTPm39n0VCX806s'
 };
 
-exports.githubToken = "fb681a3920a27a257066fcbf1043512d3f59b275";
+exports.githubToken = "65fcd2f375ab896ae91288fb292ec926c858ffe3";
 
 exports.screenName = "callouscommits";
 
