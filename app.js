69048792c17ad606699dffaf405122826929f0fc
Sameet <sameetandpotatoes@gmail.com>
2015-04-05 19:54:13 -0500

    commented app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
+//Header for the request package for the GET request to GitHub
 var headers = {
   "User-Agent": settings.screenName,
   "ETag": "a18c3bded88eb5dbb5c849a489412bf3"
@@ -18,14 +19,19 @@ var options = {
   "headers": headers,
   "auth": {
     "username": settings.githubToken + ":x-oauth-basic"
+    //With GitHub auth, I have almost 5000 API requests at a time, which is more than enough
   }
 };
 
 var bot = new TwitterBot(settings.twitterAccess);
+//The "funny" words and "bad" words
 var yes_words = settings.yesWords;
 var no_words = settings.noWords;
+//Stores previous tweets
 var last_tweets_queue = [];
 
+//Determines whether the @param message is actually funny
+//Returns true if funny, false otherwise
 function checkCommit(message) {
   var yesPattern = new RegExp(yes_words.join("|"));
   var noPattern = new RegExp(no_words.join("|"));
@@ -47,14 +53,18 @@ function parse_github_results(err, res, body) {
         var commit = r[i].message;
         var owner = r[i].author.name;
         var tweet = commit + " ~ " + owner;
-        if (last_tweets_queue.indexOf(tweet) < 0){ //New tweet
+        //Checks queue of previous tweets before tweeting this one
+        //This allows for multiple tweets by same owner (consecutively)
+        //But doesn't post the same tweet again
+        if (last_tweets_queue.indexOf(tweet) < 0){
           bot.tweet(tweet);
           console.log("Tweeted: " + tweet);
           last_tweets_queue.push(tweet);
+          //Limit of 50 on the queue, kicks off the first one added
           if (last_tweets_queue.length > 50){
             last_tweets_queue.shift();
           }
-        } else{ //Already in there, try again
+        } else{ //Already posted
 
         }
       }
@@ -62,7 +72,10 @@ function parse_github_results(err, res, body) {
   } catch (e) {
     console.log("ERROR: " + e.message);
   }
+  //Repeats this function indefinitely
   setTimeout(function () {
+    //Request package used to submit get request with @param options
+    //Callback is function parse_github_results
     request(options, parse_github_results);
   }, 5000);
 }
