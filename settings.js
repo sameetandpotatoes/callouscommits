d0324e79da622d1fd4d241b8c30453400f53c197
Sameet Sapra <sameetandpotatoes@gmail.com>
2015-07-18 13:59:52 -0500

    erased settings

--- settings.js
+++ settings.js
@@ -1,14 +1,14 @@
 exports.twitterAccess = {
-  "consumer_key": 'wXIq3R9QZjAgBllxlpzUbxYwI',
-  "consumer_secret": '23Ewmj7Ab6DZtaAdN28z1NyUQLXhwo7oJz1BpgkuTD8u9b4lZ2',
-  "access_token": '3134504248-g7NQtbwt4iZqCJ3MsbzHmkKbwbYQBgnqbuqny7N',
-  "access_token_secret": 'dUqCkEINoTN7NTjey5iDmkTK9METXEMTPm39n0VCX806s'
+  "consumer_key": '',
+  "consumer_secret": '',
+  "access_token": '',
+  "access_token_secret": ''
 };
 
-exports.githubToken = "79d32196246bd88ab5e7eb88bf3c71459d3eddc3";
+exports.githubToken = "";
 
-exports.screenName = "callouscommits";
+exports.screenName = "";
 
-exports.yesWords = ['fuck', 'shit', ' crap ', 'damn', 'bitch'];
+exports.yesWords = [''];
 
-exports.noWords = ['merge', 'branch', 'master'];
+exports.noWords = [''];
